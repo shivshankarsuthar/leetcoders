ba6d89f26cb6ff5b9087551cbd8383f63a72a2e5
shivshankar786 <shivkumar@detecttechnologies.com>
2024-06-23 11:11:02 +0530

    .

---
+++ striver/step-13 (Binary Trees)/binaryTreeFormation.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+class BinaryTree:
+    def __init__(self,data,left,right) -> None:
+        self.data = data
+        self.left = left
+        self.right = right
+
+class Solution:
+    def insertInLevel(self,)
+    def createTree(self, root, l):
+        # Code here
---
+++ striver/step-13 (Binary Trees)/maximumNodes.py
@@ -0,0 +1,4 @@
+class Solution:
+    def countNodes(self, i):
+        # Code here
+        return 2 ** (i-1)
