b26279cc3454fbbc245c91b26ee17a24d3119941
shivshankar@786 <shivkumar@detecttechnologies.com>
2024-09-16 23:49:40 +0530

    .

--- learnings/react/hello/hello/src/Components/Navbar/Navbar.js
+++ learnings/react/hello/hello/src/Components/Navbar/Navbar.js
@@ -1,5 +1,19 @@
+import react,{useState} from 'react';
+
 export default function Navbar(props){
+    const [text,setText] = useState("Enter text here!");
+    const handleTextChange = (event)=>{
+        setText(event.target.value);
+    };
+    const handleButtonClick = (event)=>{
+        setText(text.toUpperCase());
+    }
     return (
+        <>
+        <input type="text" value={text} onChange={handleTextChange} />
+        <button onClick={handleButtonClick}
+        >Capitalise</button>
         <div>This is navbar {props.title}</div>
+        </>
     );
 }
